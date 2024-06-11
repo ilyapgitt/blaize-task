b2fcc0ad8b1b1180bcb914fae73f6ee43e1336f2
Illia Ptah <ilyaptakh@gmail.com>
2024-06-11 03:58:31 +0300

    fixed CustomTooltip

--- src/app/components/Market/CustomTooltip.tsx
+++ src/app/components/Market/CustomTooltip.tsx
@@ -1,9 +1,9 @@
-export const CustomTooltip = ({ active, payload, label }) => {
+export const CustomTooltip = ({ active, payload, label }: { active: any, payload: any, label: any}) => {
   if (active && payload && payload.length) {
     return (
       <div className="custom-tooltip">
       <div>
-        {payload.map((pld) => (
+        {payload.map((pld: any) => (
           <div style={{ display: "inline-block", padding: 10, fontSize: '12px', backgroundColor: ''}}>
             <div style={{ color: pld.fill }}>{pld.value}</div>
             <div>{pld.dataKey}</div>
